9ddf0f019b477ce5a441eb31925f8b4ee056b793
Di HUANG <di.huang@ask.com>
2013-02-02 13:25:36 -0800

    Checkin

---
+++ src/main/java/facebook/hackercup/roundOne/CardGame.java
@@ -0,0 +1,12 @@
+package facebook.hackercup.roundOne;
+
+/**
+ * User: huangd
+ * Date: 2/2/13
+ * Time: 10:07 AM
+ */
+public class CardGame {
+    public static void main(String[] args){
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
